8df158594eb3ad301eb3030531d1d6f083812284
Gal Sasson <sasgal@gmail.com>
2015-11-07 21:26:26 -0500

    add express app - server

---
+++ server/routes/index.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+/* render a page */
+// router.get('/', function(req, res) {
+// 	console.log(req.query);
+
+// 	res.render('index', {
+// 		title: 'My App!',
+// 		age: 33
+// 	});
+// });
+
+/* get and respond to a post */
+// router.post("/", function(req, res) {
+// 	console.log(req.query);
+//  res.send(req.params.id,200);
+// })
+
+/* changing path like /id/12321 */
+router.get("/code", function(req, res) {
+	console.log(req.query);
+
+	res.render('code', {
+		title: 'My App!',
+		age: 33
+	});
+});
+
+router.get("/play", function(req, res) {
+	console.log(req.query);
+
+	res.render('play', {
+		title: 'My App!',
+		age: 33
+	});
+});
+
+module.exports = router;
